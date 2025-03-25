5e40b07c9eb6b22ac74b72dd270fd9feecb3ef37
Kshitij131 <1ms23cy032@msrit.edu>
2025-03-25 06:09:21 +0530

    Completed task 3

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+const button=document.getElementById('changeColorButton');
+const area=document.getElementById('colorBox');
+button.addEventListener('click', function(){
+    area.style.backgroundColor='black';
+});
